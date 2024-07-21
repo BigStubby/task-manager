4fe1a000a286f1422dbac4efa8962daf90c68335
BigStubby <tomadamik@gmail.com>
2024-07-22 00:48:10 +0200

    Revert "Task Manager with two forms"
    This reverts commit 9e72b34136c878eaa6dbb3a08823993619e9d3de.

--- task-manager/Program.cs
+++ task-manager/Program.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace task_manager
 {
-    internal static class MainProgram
+    internal static class Program
     {
         /// <summary>
         /// The main entry point for the application.
@@ -17,11 +16,7 @@ namespace task_manager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());  
+            Application.Run(new Form1());
         }
     }
-
-
-
 }
-
